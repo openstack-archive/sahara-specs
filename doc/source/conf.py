634ff6c13a36d7c78b4ee4e31bfe09e5a723237a
Sergey Lukjanov <slukjanov@mirantis.com>
2014-10-13 13:31:53 +0400

    Add redirects for implemented Juno specs
    Change-Id: I319b1648acf6d542a5090616c71068ef3a4f5afb

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,7 +18,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -27,7 +27,8 @@ import os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = ['redirect',
+              'sphinx.ext.autodoc',
               'sphinx.ext.todo',
               'sphinx.ext.viewcode',
               'oslosphinx',
